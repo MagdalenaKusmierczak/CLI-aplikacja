3330422b81384ad2187ba9e1ff7c913cfb036ec3
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-03-27 17:28:21 +0100

    details

--- contacts.js
+++ contacts.js
@@ -1,10 +1,11 @@
 const fs = require("fs").promises;
 const path = require("path");
+const {nanoid}= require("./nanoid/nanoid");
 
 const contactsPath = path.resolve("db/contacts.json");
 
 
-const fetchContacts = async () => {
+const readContacts = async () => {
   try {
     const contactsData = await fs.readFile(contactsPath);
     const contacts = JSON.parse(contactsData);
@@ -16,7 +17,7 @@ const fetchContacts = async () => {
 
 async function listContacts() {
   try {
-    const contacts = await fetchContacts();
+    const contacts = await readContacts();
     console.log("Contacts loading ended with success");
     console.table(contacts);
     return contacts;
@@ -27,7 +28,7 @@ async function listContacts() {
 
 async function getContactById(contactId) {
   try {
-    const contacts = await fetchContacts();
+    const contacts = await readContacts();
     const contact = contacts.find((contact) => contact.id === contactId);
     if (contact) {
       console.log(`Contact with id= ${contactId} is ${contact.name}`);
@@ -43,7 +44,7 @@ async function getContactById(contactId) {
 
 async function removeContact(contactId) {
   try {
-    const contacts = await fetchContacts();
+    const contacts = await readContacts();
     const newContacts = contacts.filter((element) => element.id !== contactId);
     if (JSON.stringify(newContacts) !== JSON.stringify(contacts)) {
       fs.writeFile(contactsPath, JSON.stringify(newContacts));
@@ -58,7 +59,7 @@ async function removeContact(contactId) {
 
 async function addContact(name, email, phone) {
   try {
-    const contacts = await fetchContacts();
+    const contacts = await readContacts();
     const newId = nanoid();
     const newContact = {
       id: newId,
@@ -80,43 +81,4 @@ module.exports = {
   getContactById,
   removeContact,
   addContact,
-};
-
-let urlAlphabet =
-  "useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict";
-let random = (bytes) => crypto.getRandomValues(new Uint8Array(bytes));
-let customRandom = (alphabet, size, getRandom) => {
-  let mask = (2 << (Math.log(alphabet.length - 1) / Math.LN2)) - 1;
-  let step = -~((1.6 * mask * size) / alphabet.length);
-  return () => {
-    let id = "";
-    while (true) {
-      let bytes = getRandom(step);
-      let j = step;
-      while (j--) {
-        id += alphabet[bytes[j] & mask] || "";
-        if (id.length === size) return id;
-      }
-    }
-  };
-};
-let customAlphabet = (alphabet, size) => customRandom(alphabet, size, random);
-let nanoid = (size = 21) => {
-  let id = "";
-  let bytes = crypto.getRandomValues(new Uint8Array(size));
-  while (size--) {
-    let byte = bytes[size] & 63;
-    if (byte < 36) {
-      // `0-9a-z`
-      id += byte.toString(36);
-    } else if (byte < 62) {
-      // `A-Z`
-      id += (byte - 26).toString(36).toUpperCase();
-    } else if (byte < 63) {
-      id += "_";
-    } else {
-      id += "-";
-    }
-  }
-  return id;
 };
